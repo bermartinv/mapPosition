ccbf1e0f1757e551737afb14f48bf4168ce6d90c
bermartinv <bermartinv.bermartin@gmail.com>
2021-12-26 23:38:44 +0100

    Change in map

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(   
+        function (position) {
+            const {latitude} = position.coords ;
+            const {longitude} = position.coords;
+
+            const coords = [latitude, longitude];
+
+            var map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+            }).addTo(map);
+
+            map.on('click', function (mapEvent) {
+                const {lat, lng} = mapEvent.latlng;
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth: 250,
+                            minWidth: 100,
+                            autoClose: false,
+                            closeOnClick: false,
+                            className: 'running-popup'
+                        })
+                )
+                .setPopupContent("Workout")
+                .openPopup();
+            });
+        },
+        function() {
+            alert ('Could not get your position')
+        }
+    );
